d3dbeeeeba32b98eb9c68e5da68691eb14a7b96d
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-01-12 23:09:52 +0100

    setup: add run-tests.sh

---
+++ run-tests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 CERN.
+#
+# Flask-Security-Invenio is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+python -m pytest
+eval "$(docker-services-cli up --cache ${CACHE:-redis} --env)"
