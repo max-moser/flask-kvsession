895bc1b2718d76ced07664308f88f03f447ef506
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-30 14:15:50 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- setup.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2011-2014 Marc Brinkmann.
-# SPDX-FileCopyrightText: 2013 Semion Sidorenko.
-# SPDX-FileCopyrightText: 2020 CERN.
-# SPDX-FileCopyrightText: 2024-2026 Graz University of Technology.
-# SPDX-License-Identifier: MIT
-
-from setuptools import setup
-
-setup()
